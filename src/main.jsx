1e336a28272dd37d4c18dda40feb7c978005a03e
AdamKourchi <9orachiadam@gmail.com>
2023-05-07 03:49:36 +0100

    footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Photo from './Photo.jsx'
+import Text from './Text.jsx'
 import './style.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
       <Photo />
+      <Text/>
     </React.StrictMode>,
   )
   
